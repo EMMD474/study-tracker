cf25bd13d56cbebe67bac489031820a53618f545
emmd474 <emmanueldaliso3@gmail.com>
2026-04-09 23:44:35 +0200

    fixed nextjs app router hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import "./globals.css";
 import TopNav from "@/components/TopNav";
 
@@ -29,8 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <TopNav />
-        {children}
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <TopNav />
+          {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
